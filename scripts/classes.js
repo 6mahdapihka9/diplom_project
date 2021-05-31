710084856e2f1ac3eed8442f8cd81fd6d89fbfd9
Kulesh Aleksandr <53787083+6mahdapihka9@users.noreply.github.com>
2021-05-31 17:38:08 +0300

    classes file

--- scripts/classes.js
+++ scripts/classes.js
@@ -15,17 +15,13 @@ class Dot {
                             if (this.underSurface =
                                 (!!t.simpleOperation(new Dot(_x, _y, _z), new Dot(_x, _y, 1000000)) &&
                                     t.whichSide(new Dot(_x, _y, _z))) < 0){
-                                // console.log(t, t.simpleOperation(t, new Dot(_x, _y, _z), new Dot(_x, _y, 1000000)), t.whichSide(new Dot(_x, _y, _z)) );
                                 break end;
                             }
-
-                // if (Math.floor(_y / accuracy) < Triangles.length && Math.floor(_x / accuracy) < Triangles[0].length)
-                // for (let t of Triangles[Math.floor(_y / accuracy)][Math.floor(_x / accuracy)])
-                //     this.underSurface = !!t.simpleOperation(new Dot(_x, _y, _z), new Dot(_x, _y, 1000));
 
         }
     }
 }
+
 class Dist {
     constructor(_d1, _d2) {
         this.value = Math.sqrt(Math.pow(_d2.x-_d1.x,2) + Math.pow(_d2.y-_d1.y,2) + Math.pow(_d2.z-_d1.z,2));
@@ -33,6 +29,7 @@ class Dist {
         this.dot2 = _d2;
     }
 }
+
 class Sphere {
     constructor(_x, _y, _z, _r) {
         if (_x !== undefined && _y !== undefined && _z !== undefined) {
@@ -44,7 +41,6 @@ class Sphere {
         }
     }
 }
-
 
 class Triangle {
     constructor(_a, _b, _c) {
@@ -66,11 +62,11 @@ class Triangle {
 
         // Теорема. Если в пространстве задана точка М0(х0, у0, z0), то уравнение плоскости,
         //     проходящей через точку М0 перпендикулярно вектору нормали (A, B, C) имеет вид: A(x – x0) + B(y – y0) + C(z – z0) = 0.
-        this.whichSide = (_p)=>{
+        this.whichSide = (_p) => {
             return this.n.i*(_p.x - this.a.x) + this.n.j*(_p.y - this.a.y) + this.n.k*(_p.z - this.a.z);
-        }
+        };
 
-        this.simpleOperation = (_a, _b)=>{
+        this.simpleOperation = (_a, _b) => {
             let m = _b.x-_a.x, p = _b.y-_a.y, l = _b.z-_a.z;
             if (this.n.i*m + this.n.j*p + this.n.k*l !== 0) {
                 let x, y, z, t;
@@ -81,17 +77,11 @@ class Triangle {
                 //проекция точки на плоскость
                 let newDot = new Dot(x, y, z);
                 if (isDotInsideTriangle(this.a, this.b, this.c, newDot)){
-                    // drawTriangle(this, true);
-                    // if (_a.z > z)
-                    //     drawLine(_a, newDot);
-                    // else
-                    //     drawLine(_b, newDot);
-                    // drawCircle(x,y);
                     return newDot;
                 }
             }
             return false;
-        }
+        };
     }
 }
 
